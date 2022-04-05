ad3ce0ea1a9d1011636e285ca1b02f89a388bcf4
Melvin Dijkstra <melvin.dijkstra@avinty.com>
2022-04-05 07:48:06 +0200

    Add command for listing compute clusters

---
+++ cmd/computecluster.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func init() {
+	var computeClusterCmd = &cobra.Command{
+		Use:   "computecluster",
+		Short: "Compute cluster commands",
+	}
+	rootCmd.AddCommand(computeClusterCmd)
+
+	var cmdList = &cobra.Command{
+		Use:   "list",
+		Short: "Get a list of compute clusters",
+		Args:  cobra.NoArgs,
+		RunE:  listComputeClusters,
+	}
+	computeClusterCmd.AddCommand(cmdList)
+}
+
+func listComputeClusters(_ *cobra.Command, _ []string) error {
+	clusters, err := previderClient.VirtualMachine.ComputeClusterList()
+	if err != nil {
+		return fmt.Errorf("list virtual machines: %w", err)
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Description"})
+	for _, cluster := range *clusters {
+		table.Append([]string{
+			cluster.Name,
+			cluster.Description,
+		})
+	}
+	table.Render()
+	return nil
+}
